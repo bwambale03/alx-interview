351675ccd28c54439d385fb9291b8a7778abe7e0
Edwin Bwambale <precious@kali>
2024-08-09 10:58:18 +0300

    new file:   .gitignore         modified:   0-pascal_triangle.py         new file:   package-lock.json         new file:   package.json

--- 0x00-pascal_triangle/0-pascal_triangle.py
+++ 0x00-pascal_triangle/0-pascal_triangle.py
@@ -1,32 +1,38 @@
-#!/usr/bin/python3
-"""Pascal's Triangle"""
+#!/usr/bin/python3 env
+'''
+Initialize triangle as an empty list.
+For each row from 0 to n-1:
+    Create a new row with 1 at the beginning.
+    For each element from 1 to row_index:
+        Calculate value as the sum of the two elements above.
+        Append value to the row.
+    Append 1 at the end of the row.
+    Add the row to the triangle.
+Return triangle.
 
+'''
+def generate_pascals_triangle(n):
 
-def pascal_triangle(n):
-    """
-    Generate Pascal's triangle up to the nth row as a list of lists.
-
-    :param n: The number of rows to generate.
-    :return: A list of lists representing Pascal's triangle.
-    """
-    pascal_tri = []
-
-    if n <= 0:
-        return []
+    triangle = []
 
     for i in range(n):
-        if (i == 0):
-            pascal_tri.append([1])
-        else:
-            cur_row = []
-            for j in range(i + 1):
-                if (j == 0 or j == i):
-                    cur_row.append(1)
-                else:
-                    cur_row.append(pascal_tri[i - 1][j - 1] +
-                                   pascal_tri[i - 1][j])
+        row = [1]
 
-            pascal_tri.append(cur_row)
 
-    return (pascal_tri)
-    
+        for j in range(1, i):
+            value = triangle[i-1][j-1] + triangle[i-1][j]
+
+            row.append(value)
+
+
+        if value > 0:
+            row.append(1)
+
+        triangle.append(row)
+    return triangle
+
+# example usage
+n=5
+pascals_triangle = generate_pascals_triangle(n)
+for row in pascals_triangle:
+    print(row)
