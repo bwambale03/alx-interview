7d3265e8ba235a78dc6c2605dd629669d91ba0dc
Edwin Bwambale <precious@kali>
2024-08-09 11:03:52 +0300

    new file:   .gitignore         modified:   0-pascal_triangle.py         new file:   package-lock.json         new file:   package.json

--- 0x00-pascal_triangle/0-pascal_triangle.py
+++ 0x00-pascal_triangle/0-pascal_triangle.py
@@ -1,38 +1,35 @@
-#!/usr/bin/python3
-'''
-Initialize triangle as an empty list.
-For each row from 0 to n-1:
-    Create a new row with 1 at the beginning.
-    For each element from 1 to row_index:
-        Calculate value as the sum of the two elements above.
-        Append value to the row.
-    Append 1 at the end of the row.
-    Add the row to the triangle.
-Return triangle.
+def pascal_triangle(n):
+    # Return an empty list if n <= 0
+    if n <= 0:
+        return []
 
-'''
-def generate_pascals_triangle(n):
-
+    # Initialize triangle as an empty list.
     triangle = []
 
+    # For each row from 0 to n-1:
     for i in range(n):
+        # Create a new row with 1 at the beginning.
         row = [1]
 
-
+        # For each element from 1 to row_index:
         for j in range(1, i):
+            # Calculate value as the sum of the two elements above.
             value = triangle[i-1][j-1] + triangle[i-1][j]
-
+            # Append value to the row.
             row.append(value)
 
-
-        if value > 0:
+        # Append 1 at the end of the row.
+        if i > 0:
             row.append(1)
 
+        # Add the row to the triangle.
         triangle.append(row)
+
+    # Return triangle.
     return triangle
 
-# example usage
-n=5
-pascals_triangle = generate_pascals_triangle(n)
+# Example usage
+n = 5
+pascals_triangle = pascal_triangle(n)
 for row in pascals_triangle:
     print(row)
